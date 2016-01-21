f9edf1afbabfb6c03d0e001b6ec7bd10a622a69b
Omar Sahyoun <osahyoun@gmail.com>
2016-01-21 17:34:49 +0100

    Fix action endpoint to post all data Set charset to UTF-8 Use VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
 require 'rspec'
 require 'webmock/rspec'
+require 'vcr'
+require './lib/actionkit_connector'
+
+# Use valid credentials when re-recording VCR cassettes
+#
+ENV['AK_U'] = "DummyUsername"
+ENV['AK_P'] = "DummyPassword"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data("<PASSWORD>") { ENV['AK_P'] }
+  config.filter_sensitive_data("<USERNAME>") { ENV['AK_U'] }
+end
 
