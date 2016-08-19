f413e6b54caaa65614e8cb2c3a1eae9bb16c02ae
tuuli <tuuli.pollanen@gmail.com>
2016-08-19 15:52:00 +0200

    Add spec for conflict due to duplicate email address

--- spec/lib/rest/user_spec.rb
+++ spec/lib/rest/user_spec.rb
@@ -15,11 +15,34 @@ describe ActionKitConnector::REST::User do
         address2: 'Random address'
     }}
 
+    let(:duplicate_email_params) {{
+        akid: '8244194',
+        email: 'test@example.com',
+        first_name: 'testy',
+        last_name: 'test',
+        country: 'United States',
+        city: 'San Francisco',
+        postal: '94103',
+        address1: 'Jam Factory 123',
+        address2: 'Random address'
+    }}
+
     context 'with valid parameters' do
       it 'updates the user on ActionKit and returns no content' do
         VCR.use_cassette('rest_user_success') do
           resp = client.update_user(params[:akid], params)
           expect(resp.response.class).to eq Net::HTTPNoContent
+          expect(resp.response.code.to_i).to eq 204
+        end
+      end
+    end
+
+    context 'with an email that already exists' do
+      it 'gets back errors and has a conflict status' do
+        VCR.use_cassette('rest_user_duplicate_email') do
+          resp = client.update_user(duplicate_email_params[:akid], duplicate_email_params)
+          expect(resp.response.code.to_i).to eq 409
+          expect(resp["errors"]).to include({"email"=>["Conflict on unique key 'email' for value 'test@example.com'"]})
         end
       end
     end
