79b56fc15aa6efad97235e37f61a074816fe6249
Rodrigo Pavano <rodrigopavano@gmail.com>
2016-10-25 14:37:55 -0300

    Added delete_action endpoint

--- lib/actionkit_connector/rest/action.rb
+++ lib/actionkit_connector/rest/action.rb
@@ -5,6 +5,10 @@ module ActionKitConnector
       def create_action(data)
         self.class.post('/action/', prep_options(data))
       end
+
+      def delete_action(id)
+        self.class.delete("/action/#{id}/", prep_options({}))
+      end
     end
   end
 end
