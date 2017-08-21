40ce08b7b010153dc77fa8a75b27cd2df9482b7e
Omar Sahyoun <osahyoun@gmail.com>
2017-08-21 16:27:56 +0100

    Missing trailing slash

--- lib/actionkit_connector/rest/language.rb
+++ lib/actionkit_connector/rest/language.rb
@@ -2,7 +2,7 @@ module ActionKitConnector
   module REST
     module Language
       def list_languages
-        self.class.get('/language', prep_options({}))
+        self.class.get('/language/', prep_options({}))
       end
     end
   end
